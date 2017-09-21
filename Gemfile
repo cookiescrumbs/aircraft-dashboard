986267ed1b1024ce338d7d743ead0689d1262201
Steven <stevenjamescook@gmail.com>
2017-09-21 16:53:03 +0100

    Scenario: Airspeed

--- Gemfile
+++ Gemfile
@@ -4,6 +4,11 @@ gem 'sinatra'
 gem 'thin'
 
 group :test do
+  gem 'byebug'
   gem 'capybara'
+  gem 'chromedriver-helper'
   gem 'cucumber'
+  gem 'rspec'
+  gem 'selenium-webdriver'
+  gem 'websocket-client-simple'
 end
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,5 +1,23 @@
 require 'capybara'
 require 'capybara/cucumber'
+require 'selenium/webdriver'
+require 'websocket-client-simple'
+require 'byebug'
+require 'rspec'
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
+
+@socket_server = ENV['SOCKET_SERVER'] = 'ws://localhost:3001'
+
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+
+Capybara.javascript_driver = :selenium
 
 require_relative '../../app.rb'
 
